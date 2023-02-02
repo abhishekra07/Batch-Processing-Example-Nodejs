052203115355b1c75428c2943685d301226ff547
abhishekra07 <abhira719@gmail.com>
2023-02-02 18:01:20 +0530

    Update batch.js

--- batch.js
+++ batch.js
@@ -1,8 +1,8 @@
 const Constants = require('./constanst');
 
-const batchQueue = []; //ill store batch requetes here.
-let lastBatchProcessingTime = Date.now(); //will keep track of last processing time
-let batchStatus = Constants.IDLE; //store current batch status
+const batchQueue = [] //ill store batch requetes here.
+let lastBatchProcessingTime = Date.now() //will keep track of last processing time
+let batchStatus = Constants.IDLE //store current batch status
 
 /**
  * 
@@ -11,7 +11,10 @@ let batchStatus = Constants.IDLE; //store current batch status
 exports.monitorQueue = async (queue) => {
     //If Batch is in idle state then only proceed further
     if(batchStatus === Constants.IDLE) {
-        await pushToBatchQueue(queue);
+        await pushToBatchQueue(queue)
+        if(parseInt(getNoOfSeconds()) >= process.env.BATCH_INTERVAL || batchQueue.length >= process.env.BATCH_SIZE) {
+            await processBatchRequests(queue)
+        }
     }
 }
 
@@ -22,5 +25,14 @@ pushToBatchQueue = async (queue) => {
     let spaceRemaining = process.env.BATCH_SIZE - batchQueue.length //calculate space remaining to push into batch queue
     let noOfJobsToPush = Math.min(queue.length, spaceRemaining) // calculate how many no of records to pop
     let jobs = queue.splice(0, noOfJobsToPush) // jobs to push
-    batchQueue.push(...jobs); // adding jobs to batchQueue for processing
+    batchQueue.push(...jobs) // adding jobs to batchQueue for processing
+}
+
+getNoOfSeconds = async () => {
+    return (Date.now() - lastBatchProcessingTime) / 1000
+}
+
+processBatchRequests = async (queue) => {
+    batchStatus = Constants.PROCESSING // change status to Processing to avoid interruption
+    
 }
