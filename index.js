1d8c32ce98a1fc4c8829da4ceb628d7debdda9be
abhishekra07 <abhira719@gmail.com>
2023-02-02 18:22:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ app.post('/api/print', async (req, res) => {
     res.status(200).json({message: 'Request Successfully added for Batch Processing!!!'})
 })
 
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Batch App is running on port ${process.env.PORT || 3000}`)
+})
+
 processMessage = async (msg) => {
     console.log('\x1b[33m%s\x1b[0m', `${new Date()} | Logging | ${msg}`);
 }
